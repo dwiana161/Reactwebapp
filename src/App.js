ac29b945ae15eb9141aee6f6062a1973bb0aca82
Dwiana Kamila <dwianakas@gmail.com>
2022-10-20 16:32:48 +0700

    initial react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+const App = () => {
+
+  return (
+    <h1>Hello</h1>
+  );
+}
+
+export default App;
