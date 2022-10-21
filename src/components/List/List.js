7dc538b0448544a56ceb685e5accd9d3f1b02512
Dwiana Kamila <dwianakas@gmail.com>
2022-10-21 09:56:11 +0700

    get api user detail

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { useState } from 'react';
-import { Header, Image, List } from 'semantic-ui-react'
+import { Header, Image, List } from 'semantic-ui-react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const UserList = ({userData, loading}) => {
 
     const [searchUser, setSearchUser] = useState('');
+    const navigate = useNavigate();
 
     return (
-        <List divided relaxed>
+<>
             {
                 loading ? <Header as='h1'>Loading...</Header> :
                 userData.filter((item) => {              //filter by searchPoke results
@@ -20,17 +22,20 @@ const UserList = ({userData, loading}) => {
                 }).map((item) => {
                     return(
                     <>
+                    <List divided relaxed  onClick={() => (navigate(`/users/${item.id}`))}>
                         <List.Item>
                         <Image avatar src={item.avatar} />
                         <List.Content>
                             <List.Header as='a'>{item.first_name} {item.last_name}</List.Header>
                         </List.Content>
                         </List.Item>
+                        </List>
                     </>
                     )
                 })
             }
-  </List>
+            </>
+
     )
 }
 
--- src/components/detail/detail.js
+++ src/components/detail/detail.js
@@ -1,16 +1,52 @@
 import axios from "axios";
-import React from "react";
+import React, { useCallback } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { Button, Grid } from "semantic-ui-react";
+import { useParams } from "react-router-dom";
+import { Button, Grid, Image, Item } from "semantic-ui-react";
 
 
 const Detail = () => {
+    const id = useParams();
+    console.log('id', id.id);
 
+    const [userDetail, setUserDetail] = useState();
+    const idData = id.id;
+
+    const items = [
+        {
+          childKey: 0,
+          image: '/images/wireframe/image.png',
+          header: 'Header',
+          description: 'Description',
+          meta: 'Metadata',
+          extra: 'Extra',
+        },
+      ]
+
+      const getUserDetail = useCallback(async () => {
+        const res = await axios.get(`https://reqres.in/api/users/${idData}`);
+        setUserDetail(res.data.data);
+
+        console.log('data', res.data.data);
+      },[idData])
+
+      useEffect(() => {
+        getUserDetail();
+      }, [])
 
     return (
         
-    <div>Detail</div>
+        <Item.Group>
+            <Item>
+            <Item.Image size='tiny' src={userDetail?.avatar} />
+
+            <Item.Content>
+                <Item.Header as='a'>{userDetail?.first_name} {userDetail?.last_name}</Item.Header>
+                <Item.Meta>{userDetail?.email}</Item.Meta>
+            </Item.Content>
+            </Item>
+        </Item.Group>
     )
 }
 
