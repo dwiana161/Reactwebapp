26b3009bc3fb81fb6881007cea1f59a29b267793
Dwiana Kamila <dwianakas@gmail.com>
2022-10-21 10:20:16 +0700

    login page

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -2,16 +2,87 @@ import axios from "axios";
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { Button, Grid } from "semantic-ui-react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button, Checkbox, Form } from 'semantic-ui-react'
 import UserList from "../List/List";
 
 const Login = () => {
+    const navigate = useNavigate();
 
+    const [errorMessages, setErrorMessages] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
 
-    return (
-        
-    <div>Login</div>
-    )
+    const database = [
+        {
+            username: "user1",
+            password: "user1",
+        },
+    ];
+
+    const errors = {
+        uname: "invalid username",
+        pass: "invalid password"
+      };
+
+    const handleSubmit = (event) => {
+        //Prevent page reload
+        event.preventDefault();
+    
+        let { uname, pass } = document.forms[0];
+    
+        // Find user login info
+        const userData = database.find((user) => user.username === uname.value);
+    
+        // Compare user info
+        if (userData) {
+          if (userData.password !== pass.value) {
+            // Invalid password
+            setErrorMessages({ name: "pass", message: errors.pass });
+          } else {
+            setIsSubmitted(true);
+            navigate('bookstore')
+          }
+        } else {
+          // Username not found
+          setErrorMessages({ name: "uname", message: errors.uname });
+        }
+      };
+    
+      // Generate JSX code for error message
+      const renderErrorMessage = (name) =>
+        name === errorMessages.name && (
+          <div className="error">{errorMessages.message}</div>
+        );
+    
+      // JSX code for login form
+      const renderForm = (
+    <>
+        <Form>
+            <Form.Field>
+            <label>Username</label>
+            <input type="text" name="uname" required placeholder='username' />
+            {renderErrorMessage("uname")}
+            </Form.Field>
+            <Form.Field>
+            <label>Password</label>
+            <input type="password" name="pass" required placeholder='Last Name' />
+            {renderErrorMessage("pass")}
+            </Form.Field>
+            <Form.Field>
+            </Form.Field>
+            <Button type='submit' onClick={handleSubmit}>Submit</Button>
+        </Form>
+        </>
+      );
+    
+      return (
+        <div className="app">
+          <div className="login-form">
+            <div className="title">Sign In</div>
+            {isSubmitted ? <Link to='/bookstore'></Link> : renderForm}
+          </div>
+        </div>
+      );
 }
 
 export default Login;
